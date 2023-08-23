b6fb8ed4d587f6aefcd210ef4d7df8e0c561d182
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2023-08-23 13:39:34 -0300

    inicial

---
+++ typebot-teste/src/App.js
@@ -0,0 +1,14 @@
+import { Standard } from "@typebot.io/react";
+
+const App = () => {
+  return (
+    <Standard
+      typebot="my-typebot-e29bvcn"
+      style={{ width: "100%", height: "600px" }}
+    />
+  );
+};
+
+
+
+export default App
